ff42c016f25b189653ec7659b655fe6e005ec574
swati <76866808+suvanipriyam@users.noreply.github.com>
2024-02-11 22:23:24 +0530

    mongo db connection done

--- index.js
+++ index.js
@@ -3,9 +3,41 @@ const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
 
+const { createAdapter } = require("@socket.io/mongo-adapter");
+const { MongoClient } = require("mongodb");
+const client = new MongoClient("mongodb://localhost:27017");
+
+const DB = "MyWorkingDB";
+const COLLECTION = "socket.io-adapter-events";
+
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+
+const mongoClient = new MongoClient("mongodb+srv://shivam:HkZGadQiwssADLbT@cluster0.sodtgra.mongodb.net/");
+
+// shivam | HkZGadQiwssADLbT
+
+const main = async () => {
+    await mongoClient.connect();
+
+    try {
+        await mongoClient.db(DB).createCollection(COLLECTION, {
+            capped: true,
+            size: 1e6
+        });
+        console.log("Collection created");
+    } catch (e) {
+        // collection already exists
+        console.log("Collection not created",e);
+    }
+    const mongoCollection = mongoClient.db(DB).collection(COLLECTION);
+
+    io.adapter(createAdapter(mongoCollection));
+    io.listen(3000);
+}
+
+
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
@@ -17,6 +49,8 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('server running at http://localhost:3000');
+server.listen(27017, () => {
+    console.log('server running at http://localhost:27017');
 });
+
+main().catch(console.dir)
